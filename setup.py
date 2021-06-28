39670fd743b45b2b856252ed877b32de0ded3bf6
ch-nagarjun <nagarjun.ch@outlook.com>
2021-06-28 19:25:48 +0530

    updating code owner

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='locust_wrapper',
+    version='1.0',
+    packages=['test', 'wrapper_locust'],
+    url='https://github.com/Arjun-47/locust_wrapper',
+    license='',
+    author='Nagarjun',
+    author_email='nagarjun.chittyala447@gmail.com',
+    description='Locust Wrapper'
+)
